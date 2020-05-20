c39eb6875664734aa98722400736146723e9656a
jmartins3 <jmartins@isel.pt>
2020-05-20 11:52:01 +0100

    tests for individual exercise list 8

--- aula17-weather-async/src/main/java/utils/Pair.java
+++ aula17-weather-async/src/main/java/utils/Pair.java
@@ -1,3 +1,4 @@
+package utils;
 
 public class Pair<T1,T2> {
     public final T1 first;
@@ -10,5 +11,9 @@ public class Pair<T1,T2> {
 
     public String toString() {
         return String.format("(%s,%s)", first, sec);
+    }
+
+    public static <T1,T2> Pair<T1,T2> pair(T1 t1, T2 t2) {
+        return new Pair(t1,t2);
     }
 }
--- aula17-weather-async/src/test/java/WeatherAsyncServiceTests.java
+++ aula17-weather-async/src/test/java/WeatherAsyncServiceTests.java
@@ -2,20 +2,23 @@ import model.DayInfo;
 import model.Location;
 import model.WeatherInfo;
 import org.junit.Test;
-import utils.FileRequest;
+
 import utils.HttpRequest;
+import utils.Pair;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Comparator.comparing;
+import static java.util.concurrent.CompletableFuture.allOf;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.rangeClosed;
 import static org.junit.Assert.*;
-import static queries.naive.EagerQueries.count;
-import static queries.lazy.Queries.*;
+import static utils.Pair.pair;
+import static utils.StreamUtils.zip;
 
 
 public class WeatherAsyncServiceTests {
@@ -39,7 +42,7 @@ public class WeatherAsyncServiceTests {
     }
 
     @Test
-    public void maxThermicAmplitudeAtLisbonInMarch2019() {
+    public void maxTemperatureDayAtLisbonInMarch2019() {
         LocalDate expectedDate = LocalDate.of(2019,3,15);
 
         LocalDate first = LocalDate.of(2019,3,1);
@@ -48,15 +51,16 @@ public class WeatherAsyncServiceTests {
         WeatherAsyncService weather =
                 new WeatherAsyncService(new WeatherAsyncWebApi(new HttpRequest()));
 
-        LocalDate maxAmplitudeDate = null;
+        LocalDate maxTempDate = null;
         // to complete
         // try to build just one async pipeline and do only join to that
         // pipeline
 
 
-        assertEquals(expectedDate, maxAmplitudeDate);
+        assertEquals(expectedDate, maxTempDate);
 
     }
+
 
 
 
@@ -89,6 +93,55 @@ public class WeatherAsyncServiceTests {
 
     }
 
+    @Test
+    public void maxTemperatureDayAtLisbonIn2019() {
+        // in this test we should found the hottest day in 2019 at Lisbon
+        // for this test we must send at least twelve requests
+        // to the service since the date period is limited rougly to a month
+        // to that purpose we define a list with the month days from jan. to dec.
+        Integer[]  monthDays= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+        WeatherAsyncService weather =
+                new WeatherAsyncService(new WeatherAsyncWebApi(new HttpRequest()));
+
+
+        CompletableFuture<Location> lisbon =
+                weather.search("Lisboa")
+                        .thenApply(stream ->
+                                stream
+                                        .filter(l -> l.getCountry().equalsIgnoreCase("Portugal"))
+                                        .filter(l -> l.getName().equalsIgnoreCase("Lisboa"))
+                                        .findFirst()
+                                        .get()
+                        );
+
+
+        // paralelize requests
+        CompletableFuture<CompletableFuture<Stream<DayInfo>>[]>
+                daysPerMonth2019 =
+                lisbon.thenApply(l -> {
+                    // to Complete
+                    return null;
+                });
+
+        // get the intermediary CompletableFuture results
+        CompletableFuture<Stream<DayInfo>> all2019DayInfo = null;
+        // to Complete from daysPerMonth2019.
+
+
+        // obtaing the 2019 max temp day (blocking oiperation)
+        DayInfo day = null;
+        // to complete from all2019DayInfo.
+
+
+        int expectedMaxTemp = 36;
+        LocalDate expectedDate = LocalDate.of(2019,5,30);
+
+        // validations
+        assertEquals(expectedMaxTemp, day.getMaxTemp());
+        assertEquals(expectedDate, day.getDate());
+
+
+    }
 
 }
